750383156a1c9199d69e35bb25bad4be9f7ac1b4
Nishant Rajpoot <nishant26n@Nishants-MacBook-Air.local>
2021-07-08 21:03:26 +0530

    weather-app initialisation

---
+++ src/components/CityContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CityContainer = () => {
+  return <div>City container</div>;
+};
+
+export default CityContainer;
---
+++ src/components/WeatherInfoContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WeatherInfoContainer = () => {
+  return <div>Weather Info</div>;
+};
+
+export default WeatherInfoContainer;
